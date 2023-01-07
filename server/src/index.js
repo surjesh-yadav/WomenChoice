eb05cb829b6c18f448c574a16e59241e117f4b2a
SurjeshYadavMasai <surjeshom@gmail.com>
2023-01-07 15:40:22 +0530

    Competed

--- server/src/index.js
+++ server/src/index.js
@@ -14,14 +14,14 @@ import UserController from "./controllers/UserController.js";
 import JeansController from "./controllers/JeansController.js";
 import KurtiController from "./controllers/KurtiController.js";
 import TopController from "./controllers/Top.controller.js";
-import CartMobileController from "./controllers/CartController.js";
+import CartController from "./controllers/CartController.js";
 import dbConnection from "./configs/db.js";
 
 app.use("/auth", UserController);
 app.use("/jeans", JeansController); 
 app.use("/kurtis", KurtiController);
 app.use("/tops", TopController);
-app.use("/cart", CartMobileController);
+app.use("/cart", CartController);
 
 app.listen(port, async (req, res) => {
   try {
